dfed5abcf15e0b3f10d99304b7ac7ca8a16ad32d
Alan <hcho7387@gmail.com>
2022-03-29 00:02:06 -0400

    Minor fix and updates

--- statsmanager/main/urls.py
+++ statsmanager/main/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('user_data/', views.user_data, name='user_data'),
+    path('user_profile/', views.user_profile, name='user_profile'),
     path('win_perc_calculator/', views.win_perc_calculator, name='win_perc_calculator'),
     path('summoner_info/', views.search_summoner, name='summoner_info'),
 ]
