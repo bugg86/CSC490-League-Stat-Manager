bbd0e1ac135e90ab3afc0cd333442e600b9f50ed
Alan <hcho7387@gmail.com>
2022-03-30 11:50:12 -0400

    Added champion mastery and league requests

--- statsmanager/main/views.py
+++ statsmanager/main/views.py
@@ -12,6 +12,7 @@ from riotapiutilities.consts import REGIONS
 
 RESRAPI_KEY = 'Token a7cb48f9a0645e2eb18ea44795907fb7be41dc58'
 RESTAPI_SEARCH_BY_NAME_URL = 'https://csc490-lsm-rest-api.azurewebsites.net/api/summoners/?name='
+RESTAPI_SEARCH_BY_CHAMPID_URL = 'https://csc490-lsm-rest-api.azurewebsites.net/api/champions/?championid='
 RIOT_KEY = 'RGAPI-ef3247df-953a-4361-80f7-f3f2c255b5d0'
 
 def home(request):
@@ -25,6 +26,10 @@ def home(request):
             summoners = search_summoner(summoner_name)
             
             summoner_matches = load_summoner_matches(summoners['puuid'])
+            
+            league = load_league(summoners['id'])
+            
+            mastery = load_champion_mastery(summoners['id'])
             
             return render(request, 'main/summoner_info.html', {'summoner': summoners, 'matches': summoner_matches})
 
@@ -41,7 +46,7 @@ def win_perc_calculator(request):
 
 def search_summoner(summoner_name):
     url = RESTAPI_SEARCH_BY_NAME_URL + summoner_name
-    print("=====requesting REST API=====")
+    print("=====requesting REST API for summoner=====")
     rest_response = req.get(url, headers={'Authorization': RESRAPI_KEY})
     print(rest_response.json())
     summoners_json = json.loads(rest_response.text)
@@ -53,7 +58,7 @@ def search_summoner(summoner_name):
         
         print("summoner not in database")
 
-        print("=====requesting RIOT API=====")
+        print("=====requesting RIOT API for summoner name=====")
         
         # request RIOT API for summoner info
         riot = RiotApi(RIOT_KEY, REGIONS['north_america'])
@@ -79,20 +84,48 @@ def search_summoner(summoner_name):
     # summoner exists in our database    
     return summoners_json[0]
 
+# champion mastery
+def load_champion_mastery(summoner_id):
+    
+    riot = RiotApi(RIOT_KEY, REGIONS['north_america'])
+    
+    print('=====requesting RIOT API for champion masteries=====')
+    riot_response = riot.get_champ_mastery_by_summoner_id(summoner_id)
+    # print(riot_response)
+    masteries = riot_response
+        
+    # sort champions by champion points
+    mastery_points = sorted(masteries, key=lambda x: x['championPoints'], reverse=True)
+    
+    # return top 5 champions
+    return mastery_points[:5]
+
+# league rank
+def load_league(summoner_id):
+    
+    riot = RiotApi(RIOT_KEY, REGIONS['north_america'])
+    
+    print('=====requesting RIOT API for league=====')
+    riot_response = riot.get_league_by_summoner_id(summoner_id)
+    # print(riot_response)
+    
+    return riot_response
+
 def load_summoner_matches(puuid):
     # request RIOT API for summoner matches
     # REGIONS['americas'] used because matches url does not have the same region parameter values as the summoner
     riot = RiotApi(RIOT_KEY, REGIONS['americas'])
     
-    print('=====requesting RIOT API=====')
+    print('=====requesting RIOT API for match ids=====')
     riot_response = riot.get_match_list_by_summoner_id(puuid, 0, 10)
-    
+    # print(riot_response)
     matches = dict()
     for match in riot_response:
-        print("=====requesting RIOT API=====")
-        matches[match] = riot.get_match_by_match_id(match) 
+        # print("=====requesting RIOT API for match data=====")
+        print("=====requesting RIOT API for match data for id: ", match, "=====", sep='')
+        matches[match] = riot.get_match_by_match_id(match)
     
-    return matches   
+    return matches
     
 def summoner_info(request):
     return render(request, 'main/summoner_info.html')
